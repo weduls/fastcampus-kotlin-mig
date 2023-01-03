71f920e5f64906697d009246b6e998ab2057b9d2
wedul <wedul.chul@gmail.com>
2023-01-03 22:32:02 +0900

    gradle 변경해보기

---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.7.0"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.7.22"
+
+    // 코플링에서 필수
+    // 스프링에서 특정애노테이션을 달았을 때 open을 알아서 붙여줌
+    kotlin("plugin.spring") version "1.7.22"
+    // entity생성에 필요한 기본 생성자를 만들어줌
+    kotlin("plugin.jpa") version "1.7.22"
+}
+
+group = "com.fastcampus.kotlinspring"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // kotlin스럽게 serialize, deserialize를 하게 해줌 (코틀린 사용시 필수라 생각)
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+
+    runtimeOnly("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+// 코틀린 컴파일러에 대한 설명
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
